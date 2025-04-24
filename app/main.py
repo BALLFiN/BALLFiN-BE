1b31e03692bf5dd837b85535feab648ac5c7df3f
천성윤 <1112csy@naver.com>
2025-04-24 13:15:47 +0900

    CORS origins 전체 주소 허용

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 # ✅ CORS 설정 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  # ← 이 주소 정확하게!
-    allow_credentials=True,
+    allow_origins=["*"],  # 모든 origin 허용
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -18,4 +18,3 @@ app.include_router(auth.router, prefix="/auth")
 app.include_router(user.router, prefix="/user")
 app.include_router(post.router, prefix="/posts")
 app.include_router(chat.router, prefix="/chat")
-
